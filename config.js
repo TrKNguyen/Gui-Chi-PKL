25a54d0eee4ab45ba6b095baab4c3a9151a5cb91
TrKNguyen <123048634+TrKNguyen@users.noreply.github.com>
2023-01-24 09:36:31 +0700

    Add files via upload
    HPNY

---
+++ config.js
@@ -0,0 +1,14 @@
+const nameGirl = 'Chị';
+const giftUrl = 'http://nodemy.vn';
+const eventName = 'HPNY';
+const titleCard = 'Thân gửi PKL';
+const contentCard = 'Chúc Linh năm mới thật vui vẻ với Bố với Mẹ với Bon và Sóc nhé. Bố không mua hoa thì để anh "Hái bông hoa anh cài lên tóc em" ha 😤';
+const contentCard_1 = 'Đỗ đại học, cười nhiều lên, và thật hạnh phúc nhé.'
+const contentCard_2 = 'Thương.'
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'hot-girl.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
